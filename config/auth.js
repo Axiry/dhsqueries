f9f5414d3eda8d99983bd5852f7437c3c2393fa2
Axiry <chong.shiangyih.eiffel@dhs.sg>
2016-09-14 17:18:04 +0800

    First Commit

---
+++ config/auth.js
@@ -0,0 +1,12 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'googleAuth' : {
+        'clientID'      : '367650366517-l2mgd8e27hd0ftp4hr5tartjqs5nlokk.apps.googleusercontent.com',
+        'clientSecret'  : 'cGeBmdf6P7GJ4ynhTp6lJfHR',
+        'callbackURL'   : 'http://dhsqueries.herokuapp.com/auth/google/callback'
+    }
+
+};
