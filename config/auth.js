0a76f973daefedf619fd0309ce457e06e10c0983
Axiry <chong.shiangyih.eiffel@dhs.sg>
2016-09-14 21:05:46 +0800

    Third Commit

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '367650366517-l2mgd8e27hd0ftp4hr5tartjqs5nlokk.apps.googleusercontent.com',
         'clientSecret'  : 'cGeBmdf6P7GJ4ynhTp6lJfHR',
-        'callbackURL'   : 'http://dhsqueries.herokuapp.com/auth/google/callback'
+        'callbackURL'   : 'http://dqueries.herokuapp.com/auth/google/callback'
     }
 
 };
