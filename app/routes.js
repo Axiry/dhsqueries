ffc93235961aca783bc1d7f84c640fa41419c94a
Axiry <chong.shiangyih.eiffel@dhs.sg>
2016-09-15 17:28:15 +0800

    Third Commit

--- app/routes.js
+++ app/routes.js
@@ -126,7 +126,7 @@ module.exports = function(app, passport) {
         });
     });
 
-    app.get('/home/stylesheet.css', function(req,res) {
+    app.get('/stylesheet.css', function(req,res) {
         res.sendFile('stylesheet.css');
     });
 };
